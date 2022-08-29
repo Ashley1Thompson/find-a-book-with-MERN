ed3571e10124c7fa78a7df6189c57789f9e5a734
Ashley Thompson <ashleythompson@Ashleys-MacBook-Air.local>
2022-08-29 17:12:10 -0400

    mutations and queries front end

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ type Query {
 type Mutation {
   login (email: String!, password: String!): Auth
   addUser (username: String!, email: String!, password: String!): Auth
-  saveBook (bookData: bookInput): User
+  saveBook (bookData: bookInput!): User
   removeBook (bookId: ID!): User
 }
 `
